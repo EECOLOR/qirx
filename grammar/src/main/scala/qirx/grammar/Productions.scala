c9d3dc36dca2952feba0e2c2d1d153d701820032
EECOLOR <eecolor@qirx.org>
2015-03-16 00:04:54 +0100

    Cleanup caused by new capabilities of `std`. Added a comment with reasoning for a variable.

--- grammar/src/main/scala/qirx/grammar/Productions.scala
+++ grammar/src/main/scala/qirx/grammar/Productions.scala
@@ -6,13 +6,15 @@ import psp.std.HashEq.universalEq
 
 trait Productions {
 
+  // This mutable construction was chosen to allow more freedom when defining productions. If
+  // we were to make this an abstract class and passed in the methods through the constructor
+  // you would be limited to the stuff that can be put into argument lists.
+
   private[this] var _productions = emptyValue[ExMap[Nonterminal, Element]]
 
   def productions = _productions
 
   protected[this] implicit class NonterminalOperations(nonterminal: Nonterminal) {
-    def := (element: Element):Unit =
-      // https://github.com/paulp/psp-std/issues/37
-      _productions = (_productions :+ nonterminal -> element).toExMap
+    def := (element: Element):Unit = _productions += (nonterminal, element)
   }
 }
